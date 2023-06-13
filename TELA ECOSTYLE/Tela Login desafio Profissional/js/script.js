eb297264d118e6b952f68c78567c852aa70b73eb
Luciano Giraldi <101355653+LucianoGiraldi@users.noreply.github.com>
2023-06-12 21:28:56 -0300

    Add files via upload

--- TELA ECOSTYLE/Tela Login desafio Profissional/js/script.js
+++ TELA ECOSTYLE/Tela Login desafio Profissional/js/script.js
@@ -13,44 +13,81 @@ btnSignup.addEventListener("click", function () {
 })
 
 // Função para verificar a senha
-function verificarSenha(senha) {
-    if (senha.length < 8 || senha.length > 20) {
-      return false;
-    }
-    if (!/^[a-zA-Z0-9]+$/.test(senha)) {
-      return false;
-    }
-    return true;
-  }
+// function verificarSenha(senha) {
+//     if (senha.length < 8 || senha.length > 20) {
+//       return false;
+//     }
+//     if (!/^[a-zA-Z0-9]+$/.test(senha)) {
+//       return false;
+//     }
+//     return true;
+//   }
   
-  // Selecionar o formulário de criação de conta
-  var formCadastro = document.querySelector('.second-content form');
+  // // Selecionar o formulário de criação de conta
+  // var formCadastro = document.querySelector('.second-content form');
   
-  // Adicionar um ouvinte de evento para o envio do formulário
-  formCadastro.addEventListener('submit', function(event) {
-    event.preventDefault(); // Impedir o envio do formulário
+  // // Adicionar um ouvinte de evento para o envio do formulário
+  // formCadastro.addEventListener('submit', function(event) {
+  //   event.preventDefault(); // Impedir o envio do formulário
   
-    // Obter os valores dos campos de senha
-    var senhaInput = formCadastro.querySelector('input[type="password"]');
-    var confirmarSenhaInput = formCadastro.querySelector('input[type="password"][placeholder="Confirme sua senha"]');
+  //   // Obter os valores dos campos de senha
+  //   var senhaInput = formCadastro.querySelector('input[type="password"]');
+  //   var confirmarSenhaInput = formCadastro.querySelector('input[type="password"][placeholder="Confirme sua senha"]');
   
-    var senha = senhaInput.value;
-    var confirmarSenha = confirmarSenhaInput.value;
+  //   var senha = senhaInput.value;
+  //   var confirmarSenha = confirmarSenhaInput.value;
   
-    // Verificar se as senhas são iguais
-    if (senha !== confirmarSenha) {
-      alert("As senhas não coincidem!");
-      return;
-    }
+  //   // Verificar se as senhas são iguais
+  //   if (senha !== confirmarSenha) {
+  //     alert("As senhas não coincidem!");
+  //     return;
+  //   }
   
-    // Verificar a validade da senha
-    if (!verificarSenha(senha)) {
-      alert("A senha deve ter pelo menos 8 caracteres, no máximo 20 caracteres e conter apenas letras e números.");
-      return;
-    }
+  //   // Verificar a validade da senha
+  //   if (!verificarSenha(senha)) {
+  //     alert("A senha deve ter pelo menos 8 caracteres, no máximo 20 caracteres e conter apenas letras e números.");
+  //     return;
+  //   }
   
-    // Se a senha passar pelas verificações, você pode prosseguir com o envio do formulário ou executar outras ações necessárias.
-    alert("Formulário enviado com sucesso!");
-    // formCadastro.submit(); // Descomente esta linha se quiser enviar o formulário.
-  });
+  //   // Se a senha passar pelas verificações, você pode prosseguir com o envio do formulário ou executar outras ações necessárias.
+  //   alert("Formulário enviado com sucesso!");
+  //   // formCadastro.submit(); // Descomente esta linha se quiser enviar o formulário.
+  // });
+
+  // var senhaCorreta = "admin123";
+  // var tentativas = 0;
   
+  // function verificarSenha() {
+  //   var senhaDigitada = document.getElementById("password").value;
+  
+  //   var regexSenha = /^[a-zA-Z0-9]+$/; // Expressão regular para verificar letras e números
+  //   var tamanhoMinimo = 5; // Mínimo de 5 caracteres
+  
+  //   if (senhaDigitada.match(regexSenha) && senhaDigitada.length >= tamanhoMinimo) {
+  //     if (senhaDigitada === senhaCorreta) {
+  //       alert("Senha correta! Acesso permitido.");
+  //     } else {
+  //       tentativas++;
+  //       if (tentativas === 3) {
+  //         alert("Você errou a senha três vezes. Seu acesso foi bloqueado.");
+  //       } else {
+  //         alert("Senha incorreta! Tente novamente.");
+  //       }
+  //     }
+  //   } else {
+  //     alert("Já foram feitas mais de 3 tentativas! Sua conta foi bloqueada, acesse seu email para libera-lá novamente");
+  //   }
+  // }
+  
+  // document.getElementById("signin").addEventListener("click", function (event) {
+  //   event.preventDefault();
+  //   verificarSenha();
+  // });
+  
+  // document.getElementById("signup").addEventListener("click", function (event) {
+  //   event.preventDefault();
+  //   verificarSenha();
+  // });
+  
+  
+
